caad34edd1eadc7b470fa55d0861d44123d1d600
Bakmurat <bakmuratkubanaliev@gmail.com>
2025-07-27 20:44:30 -0700

    Destroy

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -1,11 +1,11 @@
-output "fp-vpc-id" {
-  value = aws_vpc.main.id
-}
+# output "fp-vpc-id" {
+#   value = aws_vpc.main.id
+# }
 
-output "fb-pub-subnet-ids" {
-  value = aws_subnet.public.*.id
-}
+# output "fb-pub-subnet-ids" {
+#   value = aws_subnet.public.*.id
+# }
 
-output "fb-priv-subnet-ids" {
-  value = aws_subnet.private.*.id
-}
+# output "fb-priv-subnet-ids" {
+#   value = aws_subnet.private.*.id
+# }
