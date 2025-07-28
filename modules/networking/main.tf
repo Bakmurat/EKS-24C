d3ae18341d9788d6a9fbe6674af65ce74e6f835e
Bakmurat <bakmuratkubanaliev@gmail.com>
2025-07-27 19:29:03 -0700

    Added ig & rt

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -25,3 +25,35 @@ resource "aws_subnet" "fp-priv-subnet" {
     Name = "${var.project_name}-private-subnet-${count.index + 1}"
   }
 }
+
+resource "aws_internet_gateway" "fp-igw" {
+  vpc_id = aws_vpc.fp-vpc.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+resource "aws_route_table" "fp-pub-rt" {
+  vpc_id = aws_vpc.fp-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.fp-igw.id
+  }
+}
+
+resource "aws_route_table" "fp-priv-rt" {
+  vpc_id = aws_vpc.fp-vpc.id
+}
+
+resource "aws_route_table_association" "fp-pub-rt-assoc" {
+  count          = length(aws_subnet.fp-pub-subnet)
+  subnet_id      = aws_subnet.fp-pub-subnet[count.index].id
+  route_table_id = aws_route_table.fp-pub-rt.id
+}
+resource "aws_route_table_association" "fp-priv-rt-assoc" {
+  count          = length(aws_subnet.fp-priv-subnet)
+  subnet_id      = aws_subnet.fp-priv-subnet[count.index].id
+  route_table_id = aws_route_table.fp-priv-rt.id
+}
