e5d5d95df256c1f0355ff82a8f55b68f3b82e06d
Bakmurat <bakmuratkubanaliev@gmail.com>
2025-08-03 17:22:39 -0700

    added eks

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -76,12 +76,12 @@ resource "aws_security_group" "eks_cluster_sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_all_ingress_control" {
   security_group_id = aws_security_group.eks_cluster_sg.id
-  cidr_ipv4       = ["0.0.0.0/0"]
+  cidr_ipv4       = "0.0.0.0/0"
   ip_protocol        = "-1"
 }
 resource "aws_vpc_security_group_egress_rule" "allow_all_egress_control" {
   security_group_id = aws_security_group.eks_cluster_sg.id
-  cidr_ipv4       = ["0.0.0.0/0"]
+  cidr_ipv4       = "0.0.0.0/0"
   ip_protocol        = "-1"
 }
 
@@ -93,13 +93,13 @@ resource "aws_security_group" "worker-sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_all_ingress_worker" {
   security_group_id = aws_security_group.worker-sg.id
-  cidr_ipv4       = ["0.0.0.0/0"]
+  cidr_ipv4       = "0.0.0.0/0"
   ip_protocol        = "-1"
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_egress_worker" {
   security_group_id = aws_security_group.worker-sg.id
-  cidr_ipv4       = ["0.0.0.0/0"]
+  cidr_ipv4       = "0.0.0.0/0"
   ip_protocol        = "-1"
 }
 
